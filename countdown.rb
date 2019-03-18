89310f19f170ea898a8dd6cda68d7462ed8b1bcf
Troy Gooden <tro12185@gmail.com>
2019-03-18 17:09:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,11 +7,7 @@ def countdown(number)
   end 
   "HAPPY NEW YEAR!"
 end
-sleep 5 
+
 def coutdown_with_sleep(number)
-  while number > 0 
-  puts "#{number} SECOND(S)!"
-  number -= 1 
-  end 
-  "HAPPY NEW YEAR!"
+  sleep number
 end 
